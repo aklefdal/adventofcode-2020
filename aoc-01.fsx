299f9178595918c810d8c2678d42a5f45cda85f8
Alf Kåre Lefdal <alf.kare@lefdal.cc>
2020-12-01 19:40:19 +0100

    Optimized version of part 2, from ~200 ms to ~2 ms

--- aoc-01.fsx
+++ aoc-01.fsx
@@ -4,10 +4,9 @@ open System.IO
 
 // Input
 let inputFilename = "aoc-01-input.txt"
-let entries = 
+let input = 
     inputFilename 
     |> File.ReadAllLines
-    |> Array.map int
 
 // Part 1
 let crossproduct2 elements =
@@ -18,11 +17,13 @@ let crossproduct2 elements =
 let sum2Equals2020 (el1, el2) = el1 + el2 = 2020
 let multiply2 (el1, el2) = el1 * el2
 
-entries
-|> crossproduct2
-|> Seq.filter sum2Equals2020
-|> Seq.head
-|> multiply2
+let solution1 =
+    input
+    |> Array.map int
+    |> crossproduct2
+    |> Seq.filter sum2Equals2020
+    |> Seq.head
+    |> multiply2
 
 // Part 2
 let crossproduct3 elements =
@@ -34,7 +35,43 @@ let crossproduct3 elements =
 let sum3Equals2020 (el1, el2, el3) = el1 + el2 + el3 = 2020
 let multiply3 (el1, el2, el3) = el1 * el2 * el3
 
-crossproduct3 entries
-|> Seq.filter sum3Equals2020
-|> Seq.head
-|> multiply3
+let solution2 =
+    input
+    |> Array.map int
+    |> crossproduct3
+    |> Seq.filter sum3Equals2020
+    |> Seq.head
+    |> multiply3
+
+// Part 2, with some speed improvements at the cost of readability
+#time "on"
+let sortedEntries = 
+    input
+    |> Array.map int
+    |> Array.sort
+
+// The big entries are out of scope since adding them
+// together would give too high sum
+let min = sortedEntries.[0]
+let min2 = sortedEntries.[1]
+// Any entry higher than this would have to give too high sum
+let maxEntry = 2020 - min - min2
+let relevantEntry entry = entry <= maxEntry
+let relevantEntries = sortedEntries |> Array.filter relevantEntry
+
+let crossproduct3 elements =
+  seq { for el1 in elements do
+          for el2 in elements do
+            for el3 in elements do
+              yield el1, el2, el3 }
+
+let sum3Equals2020 (el1, el2, el3) = el1 + el2 + el3 = 2020
+let multiply3 (el1, el2, el3) = el1 * el2 * el3
+
+let solution2b =
+    relevantEntries
+    |> crossproduct3
+    |> Seq.filter sum3Equals2020
+    |> Seq.head
+    |> multiply3
+#time "off"
