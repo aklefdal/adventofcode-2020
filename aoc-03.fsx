a503b0f1f76dd0055c0f83af81c17dde0048d151
Alf Kåre Lefdal <alf.kare@lefdal.cc>
2020-12-03 11:48:09 +0100

    Refactored, solution 1 based upon solution 2

--- aoc-03.fsx
+++ aoc-03.fsx
@@ -1,63 +1,30 @@
 // https://adventofcode.com/2020/day/3
 
-open System.IO
+let input = "aoc-03-input.txt" |> System.IO.File.ReadAllLines
+let inputLength = input.Length
+let inputWidth = input.[0].Length
 
-// Input
-let inputFilename = "aoc-03-input.txt"
-let input = 
-    inputFilename 
-    |> File.ReadAllLines
-
-let length = input.Length
-let frameWidth = input.[0].Length
-
-let isTree pos =
-    let x, y = pos
-    if y >= length then
+let isTree (x, y) =
+    if y >= inputLength then
         None
     else
-        let x' = x % frameWidth
-        input.[y].[x'] = '#' |> Some
+        input.[y].[x % inputWidth] = '#' |> Some
 
-//
-// Part 1
-//
-let move (x, y) = x + 3, y + 1
+let move (deltaX, deltaY) (posX, posY) =  posX + deltaX, posY + deltaY
 
-let rec findTrees count pos =
-    match pos |> isTree with
-    | None -> count
-    | Some isTree ->
-        let newCount = if isTree then count + 1 else count
-        let newPos = pos |> move
-        findTrees newCount newPos
-
-let solution1 = findTrees 0 (0, 0)
-
-//
-// Part 2
-//
-let movePatterns =
-    [| 1, 1
-       3, 1
-       5, 1
-       7, 1
-       1, 2 |]
-
-let move2 (movePatternX, movePatternY) (posX, posY) =
-    posX + movePatternX, posY + movePatternY
-
-let rec findTrees2 movePattern count pos =
+let rec findTrees delta count pos =
     match pos |> isTree with
     | None -> count
     | Some isTree ->
         let newCount = if isTree then count + 1L else count
-        let newPos = pos |> move2 movePattern
-        findTrees2 movePattern newCount newPos
+        let newPos = pos |> move delta
+        findTrees delta newCount newPos
 
-let solution1b = findTrees2 (3, 1) 0L (0, 0)
+// Part 1
+let solution1 = findTrees (3, 1) 0L (0, 0)
 
+// Part 2
 let solution2 =
-    movePatterns
-    |> Array.map (fun movePattern -> findTrees2 movePattern 0L (0, 0))
-    |> Array.reduce (*)
+    [ 1, 1; 3, 1; 5, 1; 7, 1; 1, 2 ]
+    |> List.map (fun delta -> findTrees delta 0L (0, 0))
+    |> List.reduce (*)
