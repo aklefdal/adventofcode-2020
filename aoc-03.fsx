373dc68a6443c3baf209a43aa3d15bf64f46fbde
Alf Kåre Lefdal <alf.kare@lefdal.cc>
2020-12-03 14:17:30 +0100

    Refactorings

--- aoc-03.fsx
+++ aoc-03.fsx
@@ -4,38 +4,38 @@ let input = "aoc-03-input.txt" |> System.IO.File.ReadAllLines
 let inputLength = input.Length
 let inputWidth = input.[0].Length
 
+let (++) (dx, dy) (x, y) =  x + dx, y + dy
+
 let isTree (x, y) =
     if y >= inputLength then
         None
     else
         input.[y].[x % inputWidth] = '#' |> Some
 
-let move (dx, dy) (x, y) =  x + dx, y + dy
-
-let rec findTrees delta count pos =
-    match pos |> isTree with
-    | None -> count
-    | Some isTree ->
-        let newCount = if isTree then count + 1L else count
-        let newPos = pos |> move delta
-        findTrees delta newCount newPos
+let findTrees delta =
+    let rec findTreesInternal delta count pos =
+        match pos |> isTree with
+        | None -> count
+        | Some true -> findTreesInternal delta (count + 1) (pos ++ delta)
+        | Some false -> findTreesInternal delta count (pos ++ delta)
+    findTreesInternal delta 0 (0, 0)
 
 // Part 1
-let solution1 = findTrees (3, 1) 0L (0, 0)
+let solution1 = findTrees (3, 1)
 
 // Part 2
 let solution2 =
     [ 1, 1; 3, 1; 5, 1; 7, 1; 1, 2 ]
-    |> List.map (fun delta -> findTrees delta 0L (0, 0))
+    |> List.map (findTrees >> int64)
     |> List.reduce (*)
 
 // Using unfold, based upon https://github.com/codybartfast/advent-of-code/blob/main/Day03.fs
 // by https://twitter.com/codybartfast
 
-let getNextPosition (dx, dy) (x, y) =
-    match x + dx, y + dy with
+let getNextPosition delta pos =
+    match pos ++ delta with
     | (_, newY) when newY >= inputLength -> None
-    | pos -> Some ((x, y), pos)
+    | nextPos -> Some (pos, nextPos)
 
 let countTrees delta =
     (0, 0)
