ad3d6ad4ecac406cd626d6ee768e5815c0358dbf
Alf Kåre Lefdal <alf.kare@lefdal.cc>
2020-12-06 13:22:39 +0100

    Refactored a little

--- aoc-06.fsx
+++ aoc-06.fsx
@@ -1,38 +1,38 @@
 // https://adventofcode.com/2020/day/6
 
-open System
-open System.Collections.Generic
-open System.Text.RegularExpressions
+let newLine = "\r\n"
 
 let replace (old: string) (n: string) (s: string) = s.Replace(old, n)
-let splitOnChar (c: char) (s: string) = s.Split([| c |], StringSplitOptions.RemoveEmptyEntries)
-let splitOnString (delimiter: string) (s: string) = s.Split([|delimiter|], StringSplitOptions.RemoveEmptyEntries)
-let newLine = "\r\n"
-let entryDelimiter = newLine + newLine
+
+let putAllInOneLine = replace newLine ""
+
+let splitOnString (delimiter: string) (s: string) =
+    s.Split([| delimiter |], System.StringSplitOptions.RemoveEmptyEntries)
+
+let groupDelimiter = newLine + newLine
+
 let input =
     "aoc-06-input.txt" |> System.IO.File.ReadAllText
 
 let solution1 =
     input
-    |> splitOnString entryDelimiter
-    |> Seq.map (replace newLine "")
-    |> Seq.map (fun s -> s |> Seq.groupBy id |> Seq.length)
+    |> splitOnString groupDelimiter
+    |> Seq.map (putAllInOneLine >> Seq.groupBy id >> Seq.length)
     |> Seq.sum
 
 // Part 2
-
 let countForAll (group: string) =
     let peoplesAnswers = group |> splitOnString newLine
     let countOfPeopleInGroup = peoplesAnswers.Length
-    let allAnswers = peoplesAnswers |> Array.reduce (+)
-    let counts = allAnswers |> Seq.countBy id
-    counts
-    |> Seq.filter (fun (_, count) -> count = countOfPeopleInGroup)
+    group
+    |> putAllInOneLine
+    |> Seq.countBy id
+    |> Seq.map snd
+    |> Seq.filter ((=) countOfPeopleInGroup)
     |> Seq.length
-    
 
 let solution2 =
     input
-    |> splitOnString entryDelimiter
+    |> splitOnString groupDelimiter
     |> Seq.map countForAll
     |> Seq.sum
