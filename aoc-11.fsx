b32cafb4ba56280fd79ec51543e7c2d3c7fd20b7
Alf Kåre Lefdal <alf.kare@lefdal.cc>
2020-12-11 13:17:43 +0100

    Removed type - using char instead

--- aoc-11.fsx
+++ aoc-11.fsx
@@ -3,30 +3,16 @@
 let input =
     "aoc-11-input.txt" |> System.IO.File.ReadAllLines
 
-type State =
-    | Available
-    | Occupied
-    | Floor
-
-let toState c =
-    match c with
-    | 'L' -> Available
-    | '#' -> Occupied
-    | '.' -> Floor
-    | _ -> failwith "ouch"
-
 let startFloor =
     input
-    |> Array.map (fun line -> line.ToCharArray() |> Array.map toState)
-
-let charsToState (floor: char [] []): State [] [] = floor |> Array.map (Array.map toState)
+    |> Array.map (fun line -> line.ToCharArray())
 
 let height = input.Length
 let width = input.[0].Length
 
-let getValue (floor: State [] []) (x, y) = floor.[y].[x]
+let getValue (floor: char [] []) (x, y) = floor.[y].[x]
 
-let getCountOfOccupiedNearbySeats (floor: State [] []) ((x, y): int * int) =
+let getCountOfOccupiedNearbySeats (floor: char [] []) ((x, y): int * int) =
     [ x - 1, y - 1
       x - 1, y
       x - 1, y + 1
@@ -37,16 +23,16 @@ let getCountOfOccupiedNearbySeats (floor: State [] []) ((x, y): int * int) =
       x + 1, y + 1 ]
     |> List.filter (fun (a, b) -> a >= 0 && a < width && b >= 0 && b < height)
     |> List.map (getValue floor)
-    |> List.filter ((=) Occupied)
+    |> List.filter ((=) '#')
     |> List.length
 
 let getNewState state countOfOccupiedNeighbours =
     match state, countOfOccupiedNeighbours with
-    | Available, 0 -> Occupied
-    | Occupied, n when n >= 4 -> Available
+    | 'L', 0 -> '#'
+    | '#', n when n >= 4 -> 'L'
     | s, _ -> s
 
-let getNewFloor (floor: State [] []) =
+let getNewFloor (floor: char [] []) =
     floor
     |> Array.mapi (fun y ->
         Array.mapi (fun x c ->
@@ -58,7 +44,7 @@ let getNewFloor (floor: State [] []) =
             getNewState c count))
 
 let countOccupied =
-    Array.sumBy (Array.filter ((=) Occupied) >> Array.length)
+    Array.sumBy (Array.filter ((=) '#') >> Array.length)
 
 let rec solve floor =
     let newFloor = floor |> getNewFloor
@@ -75,17 +61,17 @@ let isOutside (a, b): bool =
 
 (1, 1) |> isOutside
 
-let rec see (floor: State [] []) (pos: int * int) (direction: int * int): State =
+let rec see (floor: char [] []) (pos: int * int) (direction: int * int): char =
     let posToCheck = pos ++ direction
     if posToCheck |> isOutside then
-        Floor
+        '.'
     else
         match posToCheck |> getValue floor with
-        | Occupied -> Occupied
-        | Available -> Available
-        | Floor -> see floor posToCheck direction
+        | '#' -> '#'
+        | 'L' -> 'L'
+        | _ -> see floor posToCheck direction
 
-let getCountOfOccupiedSeenSeats (floor: State [] []) ((x, y): int * int) =
+let getCountOfOccupiedSeenSeats (floor: char [] []) ((x, y): int * int) =
     [ -1, -1
       -1, 0
       -1, 1
@@ -95,13 +81,13 @@ let getCountOfOccupiedSeenSeats (floor: State [] []) ((x, y): int * int) =
       1, 0
       1, 1 ]
     |> List.map (see floor (x, y))
-    |> List.filter ((=) Occupied)
+    |> List.filter ((=) '#')
     |> List.length
 
 let getNewState2 state countOfOccupiedNeighbours =
     match state, countOfOccupiedNeighbours with
-    | Available, 0 -> Occupied
-    | Occupied, n when n >= 5 -> Available
+    | 'L', 0 -> '#'
+    | '#', n when n >= 5 -> 'L'
     | s, _ -> s
 
 let rec solve2 floor =
@@ -115,4 +101,6 @@ let rec solve2 floor =
 
     if newFloor = floor then newFloor |> countOccupied else solve2 newFloor
 
+#time "on"
 let solution2 = startFloor |> solve2
+#time "off"
