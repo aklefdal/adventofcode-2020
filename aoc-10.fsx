0b9ae8c677ec9ee81a6c4807b1e334fd2c5111bf
Alf Kåre Lefdal <alf.kare@lefdal.cc>
2020-12-11 09:59:48 +0100

    Dec 10 part two.

--- aoc-10.fsx
+++ aoc-10.fsx
@@ -17,3 +17,30 @@ let counts =
     |> dict
 
 let solution1 = (counts.[1]) * (counts.[3])
+
+// Part 2
+
+// Start with all nodes sorted from highest to lowest
+let nodes =
+    input
+    |> Array.append [| 0; max + 3 |]
+    |> Array.sortDescending
+
+// This is a folder. The state is a list of node counts, where the count
+// represent possible combinations from this node and out. Since we start
+// with the highest node, we will always find a node's descendant in the
+// state.
+let countFromHighest (nodeCounts: (int * int64)  list) (node: int) =
+    let countOfDescendants =
+        nodes // all nodes
+        |> Array.filter (fun n -> node < n && n <= node + 3 ) // descendants
+        |> Array.map (fun descendant -> nodeCounts |> List.find (fun (n, c) -> n = descendant)) // counts of each descendant
+        |> Array.map snd // just the count
+        |> Array.fold (+) 0L// sum all the counts
+        |> (fun c -> if c = 0L then 1L else c) // First entry will not have any descendants, hence the special treatment
+    (node, countOfDescendants) :: nodeCounts // add this node to the state
+
+let solution2 =
+    nodes
+    |> Array.fold countFromHighest []
+    |> List.head
