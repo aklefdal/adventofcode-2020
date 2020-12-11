4ac8aa941b0bb2921ed0a2baa94a0d56b18742ac
Alf Kåre Lefdal <alf.kare@lefdal.cc>
2020-12-11 10:29:27 +0100

    Use lists instead of array

--- aoc-10.fsx
+++ aoc-10.fsx
@@ -1,5 +1,6 @@
 // https://adventofcode.com/2020/day/10
 #time "on"
+
 let input =
     "aoc-10-input.txt"
     |> System.IO.File.ReadAllLines
@@ -19,31 +20,37 @@ let counts =
 let solution1 = (counts.[1]) * (counts.[3])
 
 // Part 2
-
 // Start with all nodes sorted from highest to lowest
 let nodes =
     input
-    |> Array.append [| 0; max + 3 |]
-    |> Array.sortDescending
+    |> Array.toList
+    |> List.append [ 0; max + 3 ]
+    |> List.sortDescending
 
-// This is a folder. The state is a list of node counts, where the count
+// This is a folder function to be used by e.g. List.fold. The state is a list of node counts, where the count
 // represent possible combinations from this node and out. Since we start
 // with the highest node, we will always find a node's descendant in the
 // state.
-let countFromHighest (nodeCounts: (int * int64)  list) (node: int) =
-    let countOfDescendants =
+let countFromHighest (nodeCounts: (int * int64) list) (node: int) =
+    let descendants =
         nodes // all nodes
-        |> Array.filter (fun n -> node < n && n <= node + 3 ) // descendants
-        |> Array.map (fun descendant -> nodeCounts |> List.find (fun (n, c) -> n = descendant)) // counts of each descendant
-        |> Array.map snd // just the count
-        |> Array.fold (+) 0L// sum all the counts
-        |> (fun c -> if c = 0L then 1L else c) // First entry will not have any descendants, hence the special treatment
+        |> List.filter (fun n -> node < n && n <= node + 3) // descendants (node number)
+        |> List.map (fun descendant ->
+            nodeCounts
+            |> List.find (fun (n, _) -> n = descendant)) // path count of each descendant
+
+    let countOfDescendants =
+        match descendants with
+        | [] -> 1L
+        | desc -> desc |> List.sumBy snd
+
     (node, countOfDescendants) :: nodeCounts // add this node to the state
 
 let solution2 =
     nodes
-    |> Array.fold countFromHighest []
+    |> List.fold countFromHighest []
     |> List.head
     |> snd
 
 #time "off"
+
