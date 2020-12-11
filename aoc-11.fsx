acdbdd9d4e87a92651a5681d8731361c9b225cca
Alf Kåre Lefdal <alf.kare@lefdal.cc>
2020-12-11 13:35:58 +0100

    Removed type annotations

--- aoc-11.fsx
+++ aoc-11.fsx
@@ -6,23 +6,40 @@ let input =
 let startFloor =
     input
     |> Array.map (fun line -> line.ToCharArray())
+    
+let countOccupied =  Array.sumBy (Array.filter ((=) '#') >> Array.length)
 
 let height = input.Length
 let width = input.[0].Length
+let isOutside (x, y) =
+    x < 0 || x >= width || y < 0 || y >= height
 
-let getValue (floor: char [] []) (x, y) = floor.[y].[x]
+let getValueFrom (floor: char [] []) (x, y) = floor.[y].[x]
 
-let getCountOfOccupiedNearbySeats (floor: char [] []) ((x, y): int * int) =
-    [ x - 1, y - 1
-      x - 1, y
-      x - 1, y + 1
-      x, y - 1
-      x, y + 1
-      x + 1, y - 1
-      x + 1, y
-      x + 1, y + 1 ]
-    |> List.filter (fun (a, b) -> a >= 0 && a < width && b >= 0 && b < height)
-    |> List.map (getValue floor)
+let (++) (dx, dy) (x, y) = x + dx, y + dy
+
+let directions =
+    [ -1, -1
+      -1, 0
+      -1, 1
+      0, -1
+      0, 1
+      1, -1
+      1, 0
+      1, 1 ]
+
+// Part 1
+
+let seeNearestNeighbour floor pos direction =
+    let posToCheck = pos ++ direction
+    if posToCheck |> isOutside then
+        '.'
+    else
+        posToCheck |> getValueFrom floor
+    
+let getCountOfOccupiedNearbySeats floor (x, y) =
+    directions
+    |> List.map (seeNearestNeighbour floor (x, y))
     |> List.filter ((=) '#')
     |> List.length
 
@@ -32,54 +49,37 @@ let getNewState state countOfOccupiedNeighbours =
     | '#', n when n >= 4 -> 'L'
     | s, _ -> s
 
-let getNewFloor (floor: char [] []) =
+let getNewFloor floor =
     floor
     |> Array.mapi (fun y ->
         Array.mapi (fun x c ->
             let pos = x, y
-
-            let count =
-                pos |> getCountOfOccupiedNearbySeats floor
-
+            let count = pos |> getCountOfOccupiedNearbySeats floor
             getNewState c count))
-
-let countOccupied =
-    Array.sumBy (Array.filter ((=) '#') >> Array.length)
 
 let rec solve floor =
     let newFloor = floor |> getNewFloor
-    if newFloor = floor then newFloor |> countOccupied else solve newFloor
+    if newFloor = floor then
+        newFloor |> countOccupied
+    else
+        solve newFloor
 
 let solution1 = startFloor |> solve
 
 // part 2
 
-let (++) (dx, dy) (x, y) = x + dx, y + dy
-
-let isOutside (a, b): bool =
-    a < 0 || a >= width || b < 0 || b >= height
-
-(1, 1) |> isOutside
-
-let rec see (floor: char [] []) (pos: int * int) (direction: int * int): char =
+let rec see floor pos direction =
     let posToCheck = pos ++ direction
     if posToCheck |> isOutside then
         '.'
     else
-        match posToCheck |> getValue floor with
+        match posToCheck |> getValueFrom floor with
         | '#' -> '#'
         | 'L' -> 'L'
         | _ -> see floor posToCheck direction
 
-let getCountOfOccupiedSeenSeats (floor: char [] []) ((x, y): int * int) =
-    [ -1, -1
-      -1, 0
-      -1, 1
-      0, -1
-      0, 1
-      1, -1
-      1, 0
-      1, 1 ]
+let getCountOfOccupiedSeenSeats floor (x, y) =
+    directions
     |> List.map (see floor (x, y))
     |> List.filter ((=) '#')
     |> List.length
@@ -101,6 +101,4 @@ let rec solve2 floor =
 
     if newFloor = floor then newFloor |> countOccupied else solve2 newFloor
 
-#time "on"
 let solution2 = startFloor |> solve2
-#time "off"
