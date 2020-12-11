6257f34b137f13396a6b72c9c70e91f5bd971f46
Alf Kåre Lefdal <alf.kare@lefdal.cc>
2020-12-11 13:41:15 +0100

    Readded type

--- aoc-11.fsx
+++ aoc-11.fsx
@@ -3,18 +3,30 @@
 let input =
     "aoc-11-input.txt" |> System.IO.File.ReadAllLines
 
+type State = 
+    | Available
+    | Occupied
+    | Floor
+
+let toState c =
+    match c with
+    | 'L' -> Available
+    | '#' -> Occupied
+    | '.' -> Floor
+    | _ -> failwith "ouch"
+    
 let startFloor =
     input
-    |> Array.map (fun line -> line.ToCharArray())
+    |> Array.map (fun line -> line.ToCharArray() |> Array.map toState)
     
-let countOccupied =  Array.sumBy (Array.filter ((=) '#') >> Array.length)
+let countOccupied =  Array.sumBy (Array.filter ((=) Occupied) >> Array.length)
 
 let height = input.Length
 let width = input.[0].Length
 let isOutside (x, y) =
     x < 0 || x >= width || y < 0 || y >= height
 
-let getValueFrom (floor: char [] []) (x, y) = floor.[y].[x]
+let getValueFrom (floor: State [] []) (x, y) = floor.[y].[x]
 
 let (++) (dx, dy) (x, y) = x + dx, y + dy
 
@@ -33,20 +45,20 @@ let directions =
 let seeNearestNeighbour floor pos direction =
     let posToCheck = pos ++ direction
     if posToCheck |> isOutside then
-        '.'
+        Floor
     else
         posToCheck |> getValueFrom floor
     
 let getCountOfOccupiedNearbySeats floor (x, y) =
     directions
     |> List.map (seeNearestNeighbour floor (x, y))
-    |> List.filter ((=) '#')
+    |> List.filter ((=) Occupied)
     |> List.length
 
 let getNewState state countOfOccupiedNeighbours =
     match state, countOfOccupiedNeighbours with
-    | 'L', 0 -> '#'
-    | '#', n when n >= 4 -> 'L'
+    | Available, 0 -> Occupied
+    | Occupied, n when n >= 4 -> Available
     | s, _ -> s
 
 let getNewFloor floor =
@@ -71,23 +83,23 @@ let solution1 = startFloor |> solve
 let rec see floor pos direction =
     let posToCheck = pos ++ direction
     if posToCheck |> isOutside then
-        '.'
+        Floor
     else
         match posToCheck |> getValueFrom floor with
-        | '#' -> '#'
-        | 'L' -> 'L'
+        | Occupied -> Occupied
+        | Available -> Available
         | _ -> see floor posToCheck direction
 
 let getCountOfOccupiedSeenSeats floor (x, y) =
     directions
     |> List.map (see floor (x, y))
-    |> List.filter ((=) '#')
+    |> List.filter ((=) Occupied)
     |> List.length
 
 let getNewState2 state countOfOccupiedNeighbours =
     match state, countOfOccupiedNeighbours with
-    | 'L', 0 -> '#'
-    | '#', n when n >= 5 -> 'L'
+    | Available, 0 -> Occupied
+    | Occupied, n when n >= 5 -> Available
     | s, _ -> s
 
 let rec solve2 floor =
