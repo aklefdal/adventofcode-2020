65f5e10fd2f7ffd8bbf2667ce46b6a8697461a17
Alf Kåre Lefdal <alf.kare@lefdal.cc>
2020-12-14 00:48:49 +0100

    Dec 13 part 2. Had to cheat by looking at the solutions of @einarwh and @object

--- aoc-13.fsx
+++ aoc-13.fsx
@@ -1,30 +1,54 @@
 // https://adventofcode.com/2020/day/13
 
-let input1 =
-    "aoc-13-input.txt"
-    |> System.IO.File.ReadAllLines
+let input =
+    "aoc-13-input.txt" |> System.IO.File.ReadAllLines
 
-let input = [| "939"; "7,13,x,x,59,x,31,19" |]
-
-let splitOnChar (c: char) (s: string) = s.Split([| c |], System.StringSplitOptions.RemoveEmptyEntries)
+let splitOnChar (c: char) (s: string) =
+    s.Split([| c |], System.StringSplitOptions.RemoveEmptyEntries)
 
 // Part 1
-let t0 = input.[0] |> int
+let t0 = input.[0] |> int64
 
 let busIds =
     input.[1]
     |> splitOnChar ','
-    |> Array.map System.Int32.TryParse
+    |> Array.map System.Int64.TryParse
     |> Array.filter fst
     |> Array.map snd
 
-let findWaitTime t0 busId =
-    busId, busId - (t0 % busId)
+let findWaitTime t0 busId = busId, busId - (t0 % busId)
 
 let waitTimes = busIds |> Array.map (findWaitTime t0)
 
 let earliestBus = waitTimes |> Array.minBy snd
+
 let solution1 =
     let (busId, waitTime) = earliestBus
     busId * waitTime
- 
+
+// Part 2
+let buses =
+    input.[1]
+    |> splitOnChar ','
+    |> Array.toList
+    |> List.mapi (fun i s -> int64 i, s)
+    |> List.filter (fun (_, s) -> s <> "x")
+    |> List.map (fun (i, s) -> i, s |> System.Int64.Parse)
+    |> List.sortBy snd
+
+let rec isValidTimeForBus (currentTime, increment) (offset, busId) =
+    if (currentTime + offset) % busId = 0L then
+        let newIncrement = increment * busId
+        (currentTime, newIncrement)
+    else
+        isValidTimeForBus (currentTime + increment, increment) (offset, busId)
+
+let (firstBus, remainingBuses) =
+    match buses with
+    | [] -> failwith "ouch"
+    | firstBus :: remainingBuses -> firstBus, remainingBuses 
+
+let solution2 =
+    remainingBuses
+    |> List.fold isValidTimeForBus (0L, firstBus |> snd)
+    |> fst
