052f0981873d0f39b9b52758a77a675a4e6c0780
Alf Kåre Lefdal <alf.kare@lefdal.cc>
2020-12-15 11:23:02 +0100

    Dec 15 Part 2 Really slow - finds solution in ~1.5 min

--- aoc-15.fsx
+++ aoc-15.fsx
@@ -12,7 +12,6 @@ let initialTurns =
     |> splitOnChar ','
     |> Array.toList
     |> List.map int
-    |> List.rev
 
 let addNextTurn turns =
     match turns with
@@ -23,13 +22,57 @@ let addNextTurn turns =
             |> List.tryFindIndex ((=) lastNumberSpoken)
             |> Option.map ((+) 1)
             |> Option.defaultValue 0
+
         thisTurn :: turns
-        
+
 let rec addNextTurns turns =
     let withNewTurn = turns |> addNextTurn
-    if withNewTurn |> List.length >= 2020 then
-        withNewTurn |> List.head
-    else
-        withNewTurn |> addNextTurns
+    if withNewTurn |> List.length >= 2020 then withNewTurn |> List.head else withNewTurn |> addNextTurns
 
-let solution1 = initialTurns |> addNextTurns 
+let solution1 = initialTurns |> List.rev |> addNextTurns
+
+// Part 2
+type State =
+    { Count: int
+      PreviousSpoken: Map<int, int>
+      LastSpoken: int }
+
+let addNextTurn2 (state: State): State =
+    let thisTurn =
+        state.PreviousSpoken
+        |> Map.tryFind state.LastSpoken
+        |> Option.map (fun lastPos -> state.Count - lastPos)
+        |> Option.defaultValue 0
+
+    { Count = state.Count + 1
+      PreviousSpoken =
+          state.PreviousSpoken
+          |> Map.add state.LastSpoken state.Count
+      LastSpoken = thisTurn }
+
+let rec addNextTurns2 max (state: State): int =
+    let withNewTurn = state |> addNextTurn2
+    if withNewTurn.Count >= max then withNewTurn.LastSpoken else withNewTurn |> addNextTurns2 max
+
+let addToState state i =
+    { Count = state.Count + 1
+      PreviousSpoken =
+          if state.Count = 0 then
+              state.PreviousSpoken
+          else
+              state.PreviousSpoken
+              |> Map.add state.LastSpoken state.Count
+      LastSpoken = i }
+
+let createState turns =
+    turns
+    |> List.fold
+        addToState
+           { Count = 0
+             PreviousSpoken = Map.empty
+             LastSpoken = 0 }
+
+let solution2 =
+    initialTurns
+    |> createState
+    |> addNextTurns2 30_000_000
